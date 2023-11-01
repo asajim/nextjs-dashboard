d6bfca619114864b5196fca8a090f936aec9bd4b
Ade Sajim <adesajim@semmtech.nl>
2023-11-01 08:03:07 +0100

    add eslint and use server action in component directly

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -67,8 +67,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
