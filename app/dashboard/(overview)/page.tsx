4ad09be9793847774c907513885496d9fb215ec9
Ade Sajim <adesajim@semmtech.nl>
2023-11-24 07:52:19 +0100

    Add fake delay

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,6 +10,7 @@ import {
 import { Suspense } from "react";
 
 export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
